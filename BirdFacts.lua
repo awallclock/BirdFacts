50765c26aa94bd550f098ce0b3a1de41714ca267
awallclock <42232299+awallclock@users.noreply.github.com>
2023-02-19 17:26:08 -0500

    Added whispers and replys
    - "/bf r" sends a bird fact to the last person you replied to. or you can start a whisper for someone and then type /bf r for the same effect
    
    -"/bf w" or "/bf t" send a bird fact to your current target. If you have no target then it just outputs a birdfact to /say

--- BirdFacts.lua
+++ BirdFacts.lua
@@ -33,6 +33,10 @@ function BirdFacts:SlashCommand(msg)
     elseif (msg == "i" or msg == "instance" or msg == "bg") then
         factChannel = "INSTANCE_CHAT"
         factOut()
+    elseif (msg == "w" or msg == "t") then
+        whisperOut()
+    elseif (msg == "r") then
+        replyOut()
     elseif (msg == "1") then
         chatChannel = "1"
         generalChatOut()
@@ -64,6 +68,8 @@ function factError()
     BirdFacts:Print("\'/bf rw\' to send a fact to /raidwarning")
     BirdFacts:Print("\'/bf i\' to send a fact to /instance")
     BirdFacts:Print("\'/bf y\' to send a fact to /yell")
+    BirdFacts:Print("\'/bf r\' to send a fact to the last person whispered")
+    BirdFacts:Print("\'/bf t\' to send a fact to your target")
     BirdFacts:Print("\'/bf <1-5>\' to send a fact to general channels")
 end
 
@@ -88,46 +94,17 @@ function generalChatOut()
     SendChatMessage(out, "CHANNEL", nil, chatChannel)
 end
 
-
-
---[[SLASH_BIRDFACTS1, SLASH_BIRDFACTS2 = '/birdfacts', '/bf'
-function SlashCmdList.BIRDFACTS(msg, editBox)
-	if (msg == "s" or msg == "say") then
-		factChannel = "SAY"
+function whisperOut()
+    local out = bFacts.fact[math.random(1, #bFacts.fact)]
+    if (UnitName("target")) then
+        SendChatMessage(out, "WHISPER", nil, UnitName("target"))
+    else
         factOut()
-    elseif (msg == "g" or msg == "guild") then
-        factChannel = "GUILD"
-        factOut()
-    elseif (msg == "p" or msg == "party") then
-        factChannel = "PARTY"
-        factOut()
-    elseif (msg == "y" or msg == "yell") then
-        factChannel = "YELL"
-        factOut()
-    elseif (msg == "rw" or msg == "raidwarning") then
-        factChannel = "RAID_WARNING"
-        factOut()
-    elseif (msg == "ra" or msg == "raid") then
-        factChannel = "RAID"
-        factOut()
-    elseif (msg == "i" or msg == "instance" or msg == "bg") then
-        factChannel = "INSTANCE_CHAT"
-        factOut()
-    elseif (msg == "1") then
-        chatChannel = "1"
-        generalChatOut()
-    elseif (msg == "2") then
-        chatChannel = "2"
-        generalChatOut()
-    elseif (msg == "3") then
-        chatChannel = "3"
-        generalChatOut()
-    elseif (msg == "4") then
-        chatChannel = "3"
-        generalChatOut()
-    elseif (msg == "5") then
-        chatChannel = "5"
-        generalChatOut()
     end
 end
---]]
+
+
+function replyOut()
+    local out = bFacts.fact[math.random(1, #bFacts.fact)]
+    SendChatMessage(out, "WHISPER", nil, ChatFrame1EditBox:GetAttribute("tellTarget"));
+end
