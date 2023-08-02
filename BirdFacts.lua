3f3937d2bec9a93d63848498acdc31c3ec3991dd
awallclock <42232299+awallclock@users.noreply.github.com>
2023-08-01 22:56:18 -0400

    Tweaked Auto-fact
    -Updated the Auto Fact output to auto output to certain channels
    -Tweaked the options menu so it's prettier :)

--- BirdFacts.lua
+++ BirdFacts.lua
@@ -11,6 +11,7 @@ local defaults = {
         realFake = "REAL",
         timerToggle = false,
         factTimer = "0",
+        defaultAutoChannel = "PARTY"
     }
 }
 
@@ -19,10 +20,17 @@ local options = {
     handler = BirdFacts,
     type = "group",
     args = {
+        generalHeader = {
+            name = "General",
+            type = "header",
+            width = "full",
+            order = 1.0
+        },
         channel = {
             type = "select",
             name = "Default channel",
             desc = "The default bird fact channel",
+            order = 1.1,
             values = {
                 ["SAY"] = "Say",
                 ["PARTY"] = "Party",
@@ -30,7 +38,7 @@ local options = {
                 ["GUILD"] = "Guild",
                 ["YELL"] = "Yell",
                 ["RAID_WARNING"] = "Raid Warning",
-                ["INSTANCE_CHAT"] = "Instance",
+                ["INSTANCE_CHAT"] = "Instance / Battleground",
                 ["OFFICER"] = "Officer"
             },
             style = "dropdown",
@@ -41,6 +49,7 @@ local options = {
             type = "select",
             name = "Fact types",
             desc = "Pick from having the option to only have real bird facts, facts about fictional birds, or both",
+            order = 1.2,
             values = {
                 ["REAL"] = "Only real facts",
                 ["FAKE"] = "Only fictional facts",
@@ -49,23 +58,49 @@ local options = {
             get = "GetRealFake",
             set = "SetRealFake",
         },
+        selfTimerHeader = {
+            name = "Auto Fact Timer",
+            type = "header",
+            width = "full",
+            order = 2.0
+        },
         factTimerToggle = {
             type = "toggle",
-            name = "Toggle Personal Auto-Fact Timer",
-            desc = "Turns on/off the Auto-Fact Timer. This will only print a message to the chatbox. No other players will see it",
+            name = "Toggle Auto-Facts",
+            order = 2.1,
+            desc =
+            "Turns on/off the Auto-Fact Timer. ",
             set = "SetToggleTimer",
             get = "GetToggleTimer",
         },
         factTimer = {
             type = "range",
-            name = "Personal Auto Fact-Timer",
-            desc = "Set the time in minutes to automatically output a bird fact. This only prints a message to the chatbox. No other players will see it",
-            order = 1,
+            name = "Auto-Fact Timer",
+            order = 2.2,
+            desc =
+            "Set the time in minutes to automatically output a bird fact.",
             min = 1,
             max = 60,
             step = 1,
             set = "SetFactTimer",
             get = "GetFactTimer",
+        },
+        autoChannel = {
+            type = "select",
+            name = "Auto-Fact channel",
+            desc = "The output channel for the Auto-Fact timer",
+            order = 2.3,
+            values = {
+                ["PARTY"] = "Party",
+                ["RAID"] = "Raid",
+                ["GUILD"] = "Guild",
+                ["RAID_WARNING"] = "Raid Warning",
+                ["INSTANCE_CHAT"] = "Instance / Battleground",
+                ["OFFICER"] = "Officer"
+            },
+            style = "dropdown",
+            get = "GetAutoMessage",
+            set = "SetAutoMessage"
         },
     }
 }
@@ -79,46 +114,57 @@ function BirdFacts:OnInitialize()
     self.optionsFrame = ACD:AddToBlizOptions("BirdFacts_options", "BirdFacts")
 end
 
-
-
 function BirdFacts:OutputFactTimer()
-self:CancelTimer(self.timer)
+    self:CancelTimer(self.timer)
     self.timeInMinutes = self.db.profile.factTimer * 60
     if self.db.profile.toggleTimer == true then
-        self.timer = self:ScheduleRepeatingTimer("SlashCommand", self.timeInMinutes, "self", "SlashCommand")
-  
+        self.timer = self:ScheduleRepeatingTimer("SlashCommand", self.timeInMinutes, "auto", "SlashCommand")
     end
 end
 
--- all the stupid get/set functions that i can't get working in the options panel, idk why they don't work
+-- all the stupid get/set functions
 function BirdFacts:GetMessage(info)
     return self.db.profile.defaultChannel
 end
+
 function BirdFacts:SetMessage(info, value)
     self.db.profile.defaultChannel = value
 end
+
+function BirdFacts:GetAutoMessage(info)
+    return self.db.profile.defaultAutoChannel
+end
+
+function BirdFacts:SetAutoMessage(info, value)
+    self.db.profile.defaultAutoChannel = value
+end
+
 function BirdFacts:GetRealFake(info)
     return self.db.profile.realFake
 end
+
 function BirdFacts:SetRealFake(info, value)
     self.db.profile.realFake = value
     BirdFacts:OutputFactTimer()
 end
+
 function BirdFacts:SetToggleTimer(info, value)
     self.db.profile.toggleTimer = value
     BirdFacts:OutputFactTimer()
 end
+
 function BirdFacts:GetToggleTimer(info)
-    return self.db.profile.toggleTimer 
+    return self.db.profile.toggleTimer
 end
+
 function BirdFacts:SetFactTimer(info, value)
     self.db.profile.factTimer = value
     BirdFacts:OutputFactTimer()
 end
+
 function BirdFacts:GetFactTimer(info)
     return self.db.profile.factTimer
 end
-
 
 -- slash commands and their outputs
 function BirdFacts:SlashCommand(msg)
@@ -166,14 +212,15 @@ function BirdFacts:SlashCommand(msg)
     elseif (msg == "opt" or msg == "options") then
         InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
         InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
-    elseif (msg == "self") then
-        BirdFacts:Print(out)
+    elseif (msg == "auto") then
+        SendChatMessage(out, self.db.profile.defaultAutoChannel)
     elseif (msg ~= "" or msg == "flags") then
         factError()
     else
         SendChatMessage(out, self.db.profile.defaultChannel)
     end
 end
+
 -- error message
 function factError()
     BirdFacts:Print("\'/bf s\' to send a fact to /say")
