738f6b54d8eb4a5be3fbc8ca39729776030cc3d5
Tim Witkovsky <tim@witkovsky.dev>
2026-04-18 21:30:54 -0400

    fixed options panel to work with classic and retail

--- BirdFacts.lua
+++ BirdFacts.lua
@@ -299,7 +299,7 @@ function BirdFacts:SlashCommand(arg)
 	end
 
 	if msg == "opt" or msg == "options" then
-		Settings.OpenToCategory(BirdFacts.categoryID)
+		BirdFacts:OpenSettings()
 		return
 	elseif msg == "auto" then
 		BirdFacts:SlashCommand(defaultAuto)
